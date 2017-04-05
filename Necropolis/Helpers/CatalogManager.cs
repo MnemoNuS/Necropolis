8f10fae27b6b3a1205151fd03963c5072d73f783
MnemoNuS <notformail@yandex.ru>
2017-04-05 20:10:44 +0300

    catalog

---
+++ Necropolis/Helpers/CatalogManager.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Necropolis.Models;
+using Newtonsoft.Json;
+using static System.Web.HttpContext;
+
+namespace Necropolis.Helpers
+{
+	public class CatalogManager
+	{
+		public const string CatalogPath = "~/JsonData/catalog.txt";
+
+		private static long lastTimeStamp = DateTime.UtcNow.Ticks;
+
+		public static long UtcNowTicks
+		{
+			get
+			{
+				long original, newValue;
+				do
+				{
+					original = lastTimeStamp;
+					long now = DateTime.UtcNow.Ticks;
+					newValue = Math.Max(now, original + 1);
+				} while (Interlocked.CompareExchange
+							 (ref lastTimeStamp, newValue, original) != original);
+
+				return newValue;
+			}
+		}
+
+		public static string generateID()
+		{
+			return Guid.NewGuid().ToString("N");
+		}
+
+		public static void SaveItem(CatalogItemModel model)
+		{
+
+			var catalog = GetCatalog();
+			var index = -1;
+			var item = model;
+			if (model.Id == null)
+				item.Id = generateID();
+
+			if (catalog.Count > 0)
+			{
+				foreach (var itemModel in catalog)
+				{
+					if (itemModel.Id.Trim() == item.Id.Trim())
+					{
+						index = catalog.IndexOf(itemModel);
+					}
+				}
+			}
+			if (index >= 0)
+			{
+				catalog[index] = item;
+			}
+			else
+			{
+				catalog.Add(item);
+			}
+			string jsonData = JsonConvert.SerializeObject(catalog, Formatting.Indented);
+			File.WriteAllText(Current.Server.MapPath(CatalogPath), jsonData);
+		}
+
+		public static void DeleteItem(string itemId)
+		{
+			var catalog = GetCatalog();
+			var index = -1;
+			if (catalog.Count > 0)
+			{
+				foreach (var itemModel in catalog)
+				{
+					if (itemModel.Id.Trim() == itemId.Trim())
+					{
+						index = catalog.IndexOf(itemModel);
+					}
+				}
+			}
+			if (index >= 0)
+			{
+				catalog.RemoveAt(index);
+			}
+			string jsonData = JsonConvert.SerializeObject(catalog, Formatting.Indented);
+			File.WriteAllText(Current.Server.MapPath(CatalogPath), jsonData);
+		}
+
+		public static CatalogItemModel GetItem(string itemId)
+		{
+			var catalog = GetCatalog();
+			var index = -1;
+			if (catalog.Count > 0)
+			{
+				foreach (var itemModel in catalog)
+				{
+					if (itemModel.Id.Trim() == itemId.Trim())
+					{
+						index = catalog.IndexOf(itemModel);
+					}
+				}
+			}
+			if (index >= 0)
+			{
+				return catalog[index];
+			}
+			return catalog[0];
+		}
+
+		public static List<CatalogItemModel> GetCatalog()
+		{
+			if (!File.Exists(Current.Server.MapPath(CatalogPath)))
+			{
+				File.Create(Current.Server.MapPath(CatalogPath));
+			}
+			var result = new List<CatalogItemModel>();
+			var data = File.ReadAllText(Current.Server.MapPath(CatalogPath));
+			var jsonData = JsonConvert.DeserializeObject<List<CatalogItemModel>>(data);
+			if (jsonData != null)
+				result.AddRange(jsonData);
+			return result;
+		}
+
+	}
+}
---
+++ Necropolis/Scripts/uploadPhotos.js
@@ -0,0 +1,47 @@
+﻿$('#changeImage').on('click', function (e) { $('#txtUploadFile').click(); });
+$('#priceEditor').on('change', function (e) {
+	var price = $('#priceEditor').val();
+	$('#price').html(price + " руб");
+});
+
+$('#txtUploadFile').on('change', function (e) {
+	var files = e.target.files;
+	var fileName = $('#txtUploadFile').attr("fileName");
+	if (files.length > 0) {
+		if (window.FormData !== undefined) {
+			var data = new FormData();
+			for (var x = 0; x < files.length; x++) {
+				data.append("file" + x, files[x]);
+			}
+
+			$.ajax({
+				type: "POST",
+				url: '/CatalogEditor/UploadFile?id=' + fileName,
+				contentType: false,
+				processData: false,
+				data: data,
+				success: function (result) {
+					console.log(result);
+					var extention = getFileExtension(files[0].name);
+					$('#Image').val("/Content/images/catalog/IMG" + fileName + "." + extention);
+					$('#ImagePreview').val("/Content/images/catalog/IMG" + fileName + "." + extention + ".[small]." + extention);
+					$('#imageLink').attr("href", "/Content/images/catalog/IMG" + fileName + "." + extention + "?timestamp=" + new Date().getTime());
+					$('#imagePreView').attr("src", "/Content/images/catalog/IMG" + fileName + "." + extention + ".[small]." + extention + "?timestamp=" + new Date().getTime());
+				},
+				error: function (xhr, status, p3, p4) {
+					var err = "Error " + " " + status + " " + p3 + " " + p4;
+					if (xhr.responseText && xhr.responseText[0] == "{")
+						err = JSON.parse(xhr.responseText).Message;
+					console.log(err);
+				}
+			});
+		} else {
+			alert("This browser doesn't support HTML5 file uploads!");
+		}
+	}
+});
+
+
+function getFileExtension(filename) {
+	return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+}
