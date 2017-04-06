6f2d1c16f96be7aaf57560944846931efd44e6d5
MnemoNuS <notformail@yandex.ru>
2017-04-06 08:57:47 +0300

    1

--- Necropolis/Controllers/CatalogController.cs
+++ Necropolis/Controllers/CatalogController.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Necropolis.Helpers;
+using Necropolis.Models;
+using Newtonsoft.Json;
+using static System.IO.File;
 
 namespace Necropolis.Controllers
 {
-    public class CatalogController : Controller
-    {
-        // GET: Catalog
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class CatalogController : Controller
+	{
+		// GET: Catalog
+		public ActionResult Index()
+		{
+			var model = CatalogManager.GetCatalog();
+			return View(model);
+		}
+	}
 }
---
+++ Necropolis/Models/CatalogItemModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Necropolis.Models
+{
+	public class CatalogItemModel
+	{
+		public string Id { get; set; }
+		public ItemType Type { get; set; }
+		public string Image { get; set; }
+		public string ImagePreview { get; set; }
+		public int Price { get; set; }
+	}
+
+	public enum ItemType
+	{
+		Кресты,
+		Надгробия
+	}
+
+
+}
